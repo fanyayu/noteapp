ea2b49bd5fd1f2c55f566e7d4903002f8934a8c1
fanyaa <ayuanggreany@gmail.com>
2018-10-05 21:23:48 +0700

    commit message

---
+++ app/src/main/java/com/fanyayu/android/mynotesapp/DatabaseContract.java
@@ -0,0 +1,17 @@
+package com.fanyayu.android.mynotesapp;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    public static String TABLE_NOTE = "note";
+
+    public static final class NoteColumns implements BaseColumns {
+
+        public static String TITLE = "title";
+
+        public static String DESCRIPTION = "description";
+
+        public static String DATE = "date";
+    }
+}
